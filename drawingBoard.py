286753b1e5a47e09d13ed51241b01bf5e069afb7
MSRayed <msrayed2006@gmail.com>
2025-07-27 14:38:46 +0600

    shapes and drawing setup

--- drawingBoard.py
+++ drawingBoard.py
@@ -1,4 +1,7 @@
 from tkinter import Canvas
+from elements import shapes
+from pymunk.vec2d import Vec2d
+
 
 class DrawingBoard(Canvas):
     def __init__(self, root, *args, **kwargs):
@@ -7,12 +10,30 @@ class DrawingBoard(Canvas):
         self.bind("<Button-1>", self.leftClick)
         self.bind("<B1-Motion>", self.leftMouseMotion)
         self.bind("<ButtonRelease-1>", self.leftMouseRelease)
+
+        self.currentElement = None
+        self.lastDrawn = None
+
+    def queue_redraw(func):
+        def wrapper(self, *args, **kwargs):
+            func(self, *args, **kwargs)
+            self.redraw()
+        return wrapper
     
     def leftClick(self, event):
-        print("left click")
-        
+        self.currentElement = shapes.Square()
+        self.currentElement.initiate(Vec2d(event.x, event.y))
+        self.currentElement.preview = True
+    
+    @queue_redraw
     def leftMouseMotion(self, event):
-        print("left mouse motion")
+        self.currentElement.release(Vec2d(event.x, event.y))
 
+    @queue_redraw
     def leftMouseRelease(self, event):
-        print("left mouse release")
+        self.currentElement.release(Vec2d(event.x, event.y))
+
+    def redraw(self):
+        if self.currentElement:
+            self.delete(self.lastDrawn)
+            self.lastDrawn = self.currentElement.draw(self)
---
+++ elements/shapes.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+
+from tkinter import Canvas
+from pymunk.vec2d import Vec2d
+
+
+class Shape(ABC):
+    def __init__(self):
+        self._fill: str = "red"
+        self._preview: bool = False
+
+    @abstractmethod
+    def initiate(self, position: Vec2d):
+        pass
+
+    @abstractmethod
+    def release(self, position: Vec2d):
+        pass
+    
+    @abstractmethod
+    def draw(self, cnv: Canvas):
+        pass
+
+    @property
+    def fill(self):
+        return self._fill
+    
+    @fill.setter
+    def fill(self, newFill):
+        self._fill = newFill
+
+    @property
+    def preview(self):
+        return self._preview
+    
+    @preview.setter
+    def preview(self, newPreview):
+        self._preview = newPreview
+
+
+class Square(Shape):
+    def __init__(self):
+        Shape.__init__(self)
+        self.topleft: Vec2d = None
+        self.bottomright: Vec2d = None
+
+    def initiate(self, position):
+        self.topleft = position
+    
+    def release(self, position):
+        self.bottomright = position
+
+    def draw(self, cnv):
+        return cnv.create_rectangle(self.topleft.x, self.topleft.y, self.bottomright.x, self.bottomright.y)
